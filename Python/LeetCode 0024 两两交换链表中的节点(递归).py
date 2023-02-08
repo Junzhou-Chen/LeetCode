2d1e6c7208d898abd138850f08d6ed5ab427cd6a
BilboJunzhou <1374709124@qq.com>
2023-02-08 19:27:08 +0800

    2023/2/8

---
+++ Python/LeetCode 0024 两两交换链表中的节点(递归).py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        newhead = head.next
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
