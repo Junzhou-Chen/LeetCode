0958a84e0bb0ec930ad6f5a84ea9f9fe7ca678db
Junzhou-chen <1374709124@qq.com>
2023-10-11 08:37:36 +0800

    Update 2023/10/11

--- Python/LeetCode 0020 有效的括号.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        if len(s)%2 != 0:
-            return False
-        while '()' in s or '[]' in s or '{}' in s:
-            s = s.replace('[]','').replace('()','').replace('{}','')
-        return True if s == '' else False
--- Python/LeetCode 0070 爬楼梯.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        p,q,r = 0,0,1
-        for i in range(n):
-            p = q
-            q = r
-            r = q + p
-        return r
-# 看着这个easy我就特来气
