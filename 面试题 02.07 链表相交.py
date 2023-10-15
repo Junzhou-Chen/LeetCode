be201c0200a55fdc059e1e61ec92d95d48a9fca4
Junzhou-chen <1374709124@qq.com>
2023-10-15 19:29:38 +0800

    LeetCode 0021

---
+++ 面试题 02.07 链表相交.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        if not headA or not headB:
+            return None
+        
+        pointerA = headA
+        pointerB = headB
+        
+        while pointerA != pointerB:
+            # 将指针向前移动一个节点
+            pointerA = pointerA.next if pointerA else headB
+            pointerB = pointerB.next if pointerB else headA
+        
+        return pointerA
+        
